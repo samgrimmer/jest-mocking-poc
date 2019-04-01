67cd0a0634ec1f531ae72fa392c1869fd6ca4881
sam grimmer <samg@iqualify.com>
2019-04-01 16:21:38 +1300

    simplified

--- Greeting.test.ts
+++ Greeting.test.ts
@@ -4,16 +4,14 @@ jest.mock('./Greeting.ts');
 
 const mockedGreeting = <jest.Mock<Greeting>>Greeting;
 
-mockedGreeting.mockImplementation(
-  (): any => {
-    return {
-      hello: jest.fn(() => 'hello there'),
-    };
+mockedGreeting.mockImplementation(() => {
+  return {
+    hello: jest.fn(() => 'hello there')
   }
-);
+})
 
-describe('Greeting tests', () => {
-  it('mocked class instatiated', () => {
+describe('Greeting class tests', () => {
+  it('mocked class instantiated', () => {
     new Greeting();
 
     expect(Greeting).toHaveBeenCalledTimes(1);
@@ -22,7 +20,6 @@ describe('Greeting tests', () => {
   it('mocked class method called', () => {
     const greeting = new Greeting().hello();
 
-    //expect(Greeting.prototype.hello).toHaveBeenCalledTimes(1);
     expect(greeting).toEqual('hello there');
   });
 });
